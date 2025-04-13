e6d8a84cb2b4e6393f2640adf7ee4914ef306278
Maruf343 <mm3677748@gmail.com>
2025-04-13 12:57:26 +0600

    instal tailwinf daisyui lucide icon

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import DaisyNav from './component/daisyNav/DaisyNav'
+import Navbar from './component/navBar/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Navbar></Navbar>
+        <DaisyNav>`</DaisyNav>
+      </header>
+      <main>
+
+      </main>
+    </>
+  )
+}
+
+export default App
